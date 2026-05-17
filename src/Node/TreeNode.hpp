a3ecf9adb5c8f8de8b56759b1fe1a3294480a9ad
Андрей <lbasalaeff@gmail.com>
2026-05-18 01:21:53 +0700

    update(cbt): move TreeNode to src/Node, remove QueueNode

--- src/Node/TreeNode.hpp
+++ src/Node/TreeNode.hpp
@@ -5,13 +5,14 @@
 // ============================================================================
 // Класс TreeNode - узел бинарного дерева. Он содержит целочисленное значение,
 // указатель на левый дочерний узел и указатель на правый дочерний узел.
-class TreeNode {
+template <typename T>
+class TreeNode {           
  public:
-  int data_;
+  T data_;
   TreeNode* left_;
   TreeNode* right_;
 
-  TreeNode(int value, TreeNode* leftNode = nullptr,
+  TreeNode(T value, TreeNode* leftNode = nullptr,
            TreeNode* rightNode = nullptr)
       : data_(value), left_(leftNode), right_(rightNode) {}
 };
