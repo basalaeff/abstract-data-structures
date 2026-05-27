0924861923f7df2f441fe6d57309f2cedd0bcfb4
Андрей <lbasalaeff@gmail.com>
2026-05-28 05:26:20 +0700

    test(sll): add SLListTest.InsertBefore, SLListTest.InsertAfter, SLListTest.InsertBeforeNotFound, SLListTest.InsertAfterNotFound, SLListTest.InsertBeforeHead

--- test/b.SLListTest.cpp
+++ test/b.SLListTest.cpp
@@ -79,5 +79,74 @@ TEST(SLListTest, RemoveFromEmptyList) {
   // Act & Assert
   EXPECT_THROW(sll.removeFromHead(), std::runtime_error);
   EXPECT_THROW(sll.removeFromTail(), std::runtime_error);
+  EXPECT_THROW(sll.insertBefore("Moscow", "London"), std::runtime_error);
+  EXPECT_THROW(sll.insertAfter("Moscow", "London"), std::runtime_error);
 }
 
+// Тест вставки перед указанным значением
+TEST(SLListTest, InsertBefore) {
+  // Arrange
+  SinglyLinkedList<std::string> sll;
+  sll.addToTail("Moscow");
+  sll.addToTail("London");
+  sll.addToTail("Paris");
+
+  // Act
+  sll.insertBefore("London", "Berlin");
+
+  // Assert
+  EXPECT_EQ(sll.getSize(), 4);
+}
+
+// Тест вставки после указанного значения
+TEST(SLListTest, InsertAfter) {
+  // Arrange
+  SinglyLinkedList<std::string> sll;
+  sll.addToTail("Moscow");
+  sll.addToTail("London");
+  sll.addToTail("Paris");
+
+  // Act
+  sll.insertAfter("London", "Berlin");
+
+  // Assert
+  EXPECT_EQ(sll.getSize(), 4);
+}
+
+// Тест вставки с ошибкой (значение не найдено)
+TEST(SLListTest, InsertBeforeNotFound) {
+  // Arrange
+  SinglyLinkedList<std::string> sll;
+  sll.addToTail("Moscow");
+  sll.addToTail("London");
+  sll.addToTail("Paris");
+
+  // Act & Assert
+  EXPECT_THROW(sll.insertBefore("Berlin", "Moscow"), std::runtime_error);
+}
+
+TEST(SLListTest, InsertAfterNotFound) {
+  // Arrange
+  SinglyLinkedList<std::string> sll;
+  sll.addToTail("Moscow");
+  sll.addToTail("London");
+  sll.addToTail("Paris");
+
+  // Act & Assert
+  EXPECT_THROW(sll.insertAfter("Berlin", "Moscow"), std::runtime_error);
+}
+
+// Тест вставки элемента перед головой
+TEST(SLListTest, InsertBeforeHead) {
+  // Arrange
+  SinglyLinkedList<std::string> sll;
+  sll.addToTail("Moscow");
+  sll.addToTail("London");
+  sll.addToTail("Paris");
+
+  // Act
+  sll.insertBefore("Moscow", "Berlin");
+
+  // Assert
+  EXPECT_EQ(sll.getSize(), 4);
+}
