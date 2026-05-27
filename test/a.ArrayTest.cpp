fb64768f77f52118df27fabe4335a0cbb68cbe58
Андрей <lbasalaeff@gmail.com>
2026-05-28 02:38:29 +0700

    test(arr): add ArrayTest.AddToTheEndResize

--- test/a.ArrayTest.cpp
+++ test/a.ArrayTest.cpp
@@ -29,7 +29,35 @@ TEST(ArrayTest, AddToTheEndWords) {
 
 // Тест для конструктора и деструктора
 TEST(ArrayTest, ConstructorAndDestructor) {
-    Array<int> arr(10);
-    EXPECT_EQ(arr.getSize(), 0);
-    EXPECT_EQ(arr.getMaxCapacity(), 10);
+  Array<int> arr(10);
+  EXPECT_EQ(arr.getSize(), 0);
+  EXPECT_EQ(arr.getMaxCapacity(), 10);
+}
+
+// Тест для добавления элемента в конец (проверка увеличения размера)
+TEST(ArrayTest, AddToTheEndResize) {
+  // Arrange (подготовка исходных данных)
+  const std::vector<std::string> words = {"Black", "White", "Red", "Green",
+                                          "Blue"};
+  // capcity: size_t -> int
+  const int capacity = static_cast<int>(words.size());
+  Array<std::string> array(capacity);
+
+  // Assert (проверка результата)
+  EXPECT_EQ(array.length(), 0);
+
+  // Act (запуск метода/функции)
+  for (int i = 0; i < capacity; ++i) {
+    array.addToTheEnd(words[i]);
+
+    // Assert (проверка результата)
+    EXPECT_EQ(array.length(), i + 1);
+    EXPECT_EQ(array.get(i), words[i]);
+  }
+
+  // Act (запуск метода/функции)
+  array.addToTheEnd("Yellow");
+  const int index = capacity;
+  EXPECT_EQ(array.get(index), "Yellow");
+  EXPECT_EQ(array.getSize(), capacity + 1);
 }
